8137903cdc16e5fed76a51157d3b4a30372c1ce8
tatparya <tatparya.shankar@gmail.com>
2015-04-23 00:08:59 -0400

    Addded project.c and .h

---
+++ project3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//	Header File
+#include "project3.h"
+
+//	~~~~~~~~~~~~~~~~~~~	Main ~~~~~~~~~~~~~~~~~~~
+int main( int * argc, char * * argv )
+{
+	
+	return 0;
+}
---
+++ project3.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//	~~~~~~~~~~~~~~~~~~~ Person Structure ~~~~~~~~~~~~~~~~~~~
+typedef struct Person {
+
+	int id;				//	Person ID
+
+	//	Person Attributes
+	int age;
+	int gender;
+	int maritalStatus;
+	int race;
+	int birthPlace;
+	int language;
+	int occupation;
+	int income;
+
+}	User;
